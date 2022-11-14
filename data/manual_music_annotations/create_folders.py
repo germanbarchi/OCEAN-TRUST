8b681b86a5e5e29c812fd64b042ae3def14fa138
germanbarchi <germanbarchi@gmail.com>
2022-11-14 12:46:15 -0300

    manual_annotations files

--- data/manual_music_annotations/create_folders.py
+++ data/manual_music_annotations/create_folders.py
@@ -1,20 +1,21 @@
 from email.mime import audio
 import os
 import shutil
+import glob 
+from IPython import embed 
+from pathlib import Path 
 
-audios_list='yamnet_music_0.2.txt'
-cwd=os.path.abspath(os.getcwd())
+audios_list='../lists/silero_valid_split_no_speech_greater_2sec.txt'
+cwd=Path(os.getcwd())
 
-audio_path=os.path.join('/'.join(cwd.split('/')[:-1]),'OCEAN_new_structure/data/audio/')
+audio_path=os.path.join(cwd.parents[2],'OCEAN_new_structure/data/audio/')
+embed()
 
-out_audios_path=os.path.join(cwd,'audio')
-
-if not os.path.exists(out_audios_path):
-    os.mkdir(out_audios_path)
+os.mkdir('audio')
 
 with open (audios_list) as file:
     files=file.read().splitlines()
 
 for file in files:
-    shutil.copy2(os.path.join(audio_path,file+'.wav'), out_audios_path)
+    shutil.copy2(glob.glob(audio_path+'*/'+file)[0], 'audio')
 
