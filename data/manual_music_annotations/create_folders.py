caf336334e8cac863a1bd933d1ed9d7ece844165
germanbarchi <germanbarchi@gmail.com>
2022-11-14 13:58:47 -0300

    manual annotations music

--- data/manual_music_annotations/create_folders.py
+++ data/manual_music_annotations/create_folders.py
@@ -8,10 +8,10 @@ from pathlib import Path
 audios_list='../lists/silero_valid_split_no_speech_greater_2sec.txt'
 cwd=Path(os.getcwd())
 
-audio_path=os.path.join(cwd.parents[2],'OCEAN_new_structure/data/audio/')
-embed()
+audio_path=os.path.join(cwd.parents[2],'OCEAN_new_structure/data/silero/silero_no_speech/')
 
-os.mkdir('audio')
+if not os.path.exists('audio'):
+    os.mkdir('audio')
 
 with open (audios_list) as file:
     files=file.read().splitlines()
--- data/manual_music_annotations/manual_labeling.py
+++ data/manual_music_annotations/manual_labeling.py
@@ -1,60 +1,64 @@
 import os
 from playsound import playsound 
+import glob
+import pandas as pd 
 
-
-def load_list (input_list):
-    with open(yamnet_music_list) as yamnet_list:
-        list=yamnet_list.read().splitlines()
-    return list
-
-def start(results_path,audios_list,audio_path):
+def start(results_path,audio_path):
+    
+    df_results=pd.DataFrame()
+    audios_list=glob.glob(audio_path+'/*.wav')
 
     print('------------------- Start -----------------------')
     print('* Ingrese 1 para muestras que contienen música \n* Ingrese 0 para muestras que no contienen música')
     print('-------------------------------------------------')
-
+    
     with open(results_path+'/music_list.txt','w') as results_music:
         with open(results_path+'/no_music_list.txt','w') as results_no_music:
             i=1
             input_error=False
             input('ENTER para comenzar')
-            for audio_sample in audios_list[:10]:                
-                
-                print('Progreso: %d/%d --> Audio sample:%s' %(i,len(audios_list),audio_sample.split('/')[1]))
-                playsound(audio_path+audio_sample)
+            for k, audio_sample in enumerate(audios_list):                
+                filename=audio_sample.split('/')[-1]
+                print('Progreso: %d/%d --> Audio sample:%s' %(i,len(audios_list),filename))
+                playsound(audio_sample)
                 
                 user_input=int(input('Ingrese respuesta:'))             
                 
                 if user_input==1:
-                    results_music.write(audio_sample+'\n')
+                    results_music.write(filename+'\n')
+                    df_results.loc[k,'music']=1
+                    df_results.loc[k,'name']=filename
                 elif user_input==0:
-                    results_no_music.write(audio_sample+'\n')
+                    results_no_music.write(filename+'\n')
+                    df_results.loc[k,'music']=0
+                    df_results.loc[k,'name']=filename
                 else:
                     input_error=True                    
                     while input_error:
                         print('Valor incorrecto. Por favor, ingrese 0 o 1')
                         user_input=int(input('Ingrese respuesta:'))   
                         if user_input==0:
-                            results_music.write(audio_sample+'\n')
+                            results_no_music.write(filename+'\n')
+                            df_results.loc[k,'music']=0
+                            df_results.loc[k,'name']=filename
                             input_error=False 
                         elif user_input==1:
-                            results_no_music.write(audio_sample+'\n')                           
+                            results_music.write(filename+'\n')
+                            df_results.loc[k,'music']=1
+                            df_results.loc[k,'name']=filename                           
                             input_error=False                  
-                i+=1                
+                i+=1
+                df_results.to_csv('df_music_labels.csv')                
                 print('-------------------------------------------------')
 
 if __name__=='__main__':
 
-    yamnet_music_list='yamnet_music_0.2.txt'
-    audios_list=load_list(yamnet_music_list)
-
     audio_path='audio/'    
  
-    cwd=os.path.abspath(os.getcwd())
-    results_path=os.path.join(cwd,'classification_results')
+    results_path='classification_results'
 
     if not os.path.exists(results_path):
         os.mkdir(results_path)
 
-    start(results_path,audios_list,audio_path)
+    start(results_path,audio_path)
 
