5abdf64a9150e18c98d736062c6c62a8cccc766a
germanbarchi <germanbarchi@gmail.com>
2022-12-01 15:01:59 -0300

    add_make_folds to utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np 
 
 def concat_df(DF_features,DF_labels):
 
@@ -53,3 +54,68 @@ def normalize_data(df,feature_tags):
         df[i]=df[i]/df[i+'_(quantile)']    
     
     return df[df.columns]
+
+def make_partitions(df,
+                    partitions,
+                    stratify_columns,
+                    independent_columns=None,
+                    random_seed=None):
+
+    np.random.seed(random_seed)
+
+    if isinstance(partitions, int):
+        partitions = [1.0 / partitions for p in range(partitions - 1)]
+    elif sum(partitions) >= 1:
+        raise Exception('Partitions proportions must sum less than 1')
+    partitions.append(1 - sum(partitions))
+
+    n_sets = len(partitions)
+
+    partidx = {i: [] for i in range(n_sets)}
+
+    if independent_columns is not None:
+        groups = df.groupby(stratify_columns).groups.items()
+        dist = {k: len(v) * np.array(partitions) for k, v in groups}
+        part = {k: np.zeros(n_sets) for k, v in groups}
+
+        for k, g in tqdm(df.groupby(independent_columns)):
+            diffs = []
+            for i, r in g.iterrows():
+                group = tuple(r[stratify_columns].to_list())
+                diffs.append(dist[group] - part[group])
+            pix = np.max(np.array(diffs), 0).argmax()
+            gix = np.max(np.array(diffs), 1).argmax()
+            partidx[pix].extend(g.index)
+            group = tuple(g.iloc[gix][stratify_columns].to_list())
+            part[group][pix] += len(g)
+    else:
+        for k, g in df.groupby(stratify_columns):
+            ix = np.tile(np.arange(n_sets),len(g)//n_sets)
+            ix = np.hstack([ix,np.random.choice(np.arange(n_sets),len(g)-len(ix), replace=False)])
+#             ix = np.random.permutation(
+#                 sum([[i] * int(np.ceil(len(g) * p))
+#                      for i, p in enumerate(partitions)], []))[:len(g)]
+            for i in range(n_sets):
+                partidx[i].extend(g[ix == i].index)
+    return partidx
+
+def make_parts(n_groups,df):
+    
+    df_=pd.DataFrame()
+    
+    for i in range(n_groups):
+        
+        df=df.sample(frac=1)
+        
+        nquant = 4
+        boundaries = df['labels_mean'].quantile(np.linspace(0,1,nquant))
+        df['labels_mean_quant'] = np.searchsorted(boundaries, df['labels_mean'])
+
+        pix = make_partitions(df,5, ['ethnicity','gender','music','labels_mean_quant'], independent_columns='basename')
+
+        for k,v in pix.items():
+            df.loc[v,'partition'] = int(k)
+            df.loc[v,'group'] = int(i)
+        df_=df_.append(df)
+    
+    return df_
