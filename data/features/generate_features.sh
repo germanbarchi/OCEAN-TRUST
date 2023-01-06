2e6d83e81a514ddaaeb1a7ca25831f882dff8d0d
germanbarchi <germanbarchi@gmail.com>
2023-01-06 14:29:37 -0300

    experiments

---
+++ data/features/generate_features.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COMPLETAR DIRECTORIO DE AUDIOS
+
+python extract_egemaps.py ../data/features/egemaps_all.csv --files_path ../data/>(directorio de audios) --normalize False
+python extract_egemaps.py ../data/features/egemaps_speech.csv --files_path ../data/silero/silero_speech --normalize False
+python extract_egemaps.py ../data/features/egemaps_no_speech.csv --files_path ../data/silero/silero_no_speech --normalize False
+
+python extract_egemaps.py ../data/features/egemaps_normalized_s.csv --files_path ../data/silero/silero_speech --normalize True
+python extract_egemaps.py ../data/features/egemaps_normalized_ns.csv --files_path ../data/silero/silero_no_speech --normalize True
